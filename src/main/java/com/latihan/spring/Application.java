3cd7ce12f713317fb9bfb76f7d0d698839d6ae68
rifan-aja <andirifan@proton.me>
2026-09-20 05:02:24 +0700

    delete: remove jdbc and spring security for learning

--- src/main/java/com/latihan/spring/Application.java
+++ src/main/java/com/latihan/spring/Application.java
@@ -2,12 +2,22 @@ package com.latihan.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController 
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping
+	public String helloWorld(){
+		return "Halo dunia";
 	}
 
 }
